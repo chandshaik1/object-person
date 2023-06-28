befc29d3c25034e87b85bee825f14c3a7e619353
CHAND BASHA SHAIK <shaik.chand.1340124@gmail.com>
2023-06-28 09:36:06 +0000

    object-person

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// App.js
+
+const person = {
+  id: 2,
+  gender: 'mail',
+}
+
+const student = {
+  name: 'ravi',
+  email: 'ravi11@yopmail.com',
+}
+
+console.log('Person:', person)
+console.log('Student:', student)
